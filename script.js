3b7ee843ad8beda3aea9417d02b6023d5e8750bd
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-12-14 12:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn = document.getElementById("submit");
+btn.addEventListener("click",()=>{
+	let size = document.getElementById("fontsize").value;
+	let color = document.getElementById("fontcolor").value;
+	let cook = document.cookie = `Fontsize = ${size}`;
+	let colorCook = document.cookie = `fontcolor = ${color}`;
+	document.getElementById("fontsize").style.value = size;
+	document.getElementById("fontcolor").style.value = color;
+})
